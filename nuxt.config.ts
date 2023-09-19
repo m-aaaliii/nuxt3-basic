7d5ed69cd44cacf8b8cfe89049278b58ed592628
aaaliii-m <muhammad.ali@ki5.co.uk>
2023-09-19 11:40:53 +0500

    initial boilerplate

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    assets: '/<rootDir>/assets'
+  },
+  css: [
+    "~/assets/main.scss"
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }, 
+})
