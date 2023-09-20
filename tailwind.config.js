cdb045a4a2e886892455a4ecd9128853642110fd
henakhaleeq <hena.khaleeq@ki5.co.uk>
2023-09-20 12:39:52 +0500

    dev-hk best seller component added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,35 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '752px',
+          },
+          '@screen lg': {
+            maxWidth: '976px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+        }
+      })
+    },
+    function ({ addVariant }) {
+      addVariant('mobile-only', "@media screen and (max-width: 767px)"); // instead of hard-coded 640px use sm breakpoint value from config. Or anything
+    },
+    function ({ addVariant }) {
+      addVariant('Tab-only', "@media screen and (min-width: 768px) and (max-width: 1023px)"); // instead of hard-coded 
+    },
+    function ({ addVariant }) {
+      addVariant('Lap-only', "@media screen and (min-width: 1024px) and (max-width: 1279px)"); // instead of hard-coded 
+    },
+  ],
 }
 
